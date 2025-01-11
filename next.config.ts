5ff1572fe07710bbc599946b2bb28b21e1de93a5
Hassan RJ <142681707+HassanRJ-3108@users.noreply.github.com>
2025-01-11 16:45:23 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
-  },
+
   images: {
     unoptimized: true,
     domains: ["cdn.sanity.io"],
